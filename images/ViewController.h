f0cefe75202523705ab13fca8d836cd0b531908a
wax2045 <861412028@qq.com>
2017-09-25 16:46:45 +0800

    init
    sfe

---
+++ images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  images
+//
+//  Created by FUWANG on 2017/9/25.
+//  Copyright © 2017年 FUWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ images/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  images
+//
+//  Created by FUWANG on 2017/9/25.
+//  Copyright © 2017年 FUWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
